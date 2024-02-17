8f163fb811c1e6248e55f6ef72f1f2ab356cd047
Dennis Moldovan <dennismoldovan32@gmail.com>
2024-02-17 19:26:17 +0200

    chore - analytics

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,7 +1,13 @@
 import { redis } from "@/lib/redis";
+import { getDate } from "@/utils";
+import { parse } from "date-fns";
 
 type AnalyticsArgs = {
     retention?: number;
+}
+
+type TrackOptions = {
+    persist?: boolean;
 }
 
 export class Analytics {
@@ -11,10 +17,50 @@ export class Analytics {
         if(opts?.retention) this.retention = opts.retention;
     }
 
-    async track(namespace: string, event: object = {}){
-        const key = `analytics::${namespace}`;
+    async track(namespace: string, event: object = {}, opts?: TrackOptions){
+        let key = `analytics::${namespace}`;
+        
+        if(!opts?.persist){
+            key += `::${getDate()}`
+        }
 
         await redis.hincrby(key, JSON.stringify(event), 1);
+    
+        if(!opts?.persist) await redis.expire(key, this.retention);
+    }
+
+    async retrieveDays(namespace: string, nDays: number) {
+        type AnalyticsPromise = ReturnType<typeof analytics.retrieve>;
+
+        const promises: Array<AnalyticsPromise> = [];
+        for(let i = 0; i < nDays; i++){
+            const formattedDate = getDate(i);
+            const promise = analytics.retrieve(namespace, formattedDate);
+        
+            promises.push(promise);
+        }
+
+        const fetched = await Promise.all(promises);
+
+        const data = fetched.sort((a, b) => {
+            if(parse(a.date, "dd/MM/yyyy", new Date()) > parse(b.date, "dd/MM/yyyy", new Date()))
+                return 1;
+
+            return -1;
+        });
+
+        return data;
+    } 
+
+    async retrieve(namespace: string, date: string) {
+        const res = await redis.hgetall<Record<string, string>>(`analytics::${namespace}::${date}`);
+    
+        return {
+            date,
+            events: Object.entries(res ?? []).map(([key, value])=>({
+                [key]: Number(value), 
+            })),
+        }
     }
 }
 
